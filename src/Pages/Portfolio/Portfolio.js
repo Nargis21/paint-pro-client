9b0ab8dff7ebc0795ee1139e76d8d2bed04274da
Nargis21 <nargisakther1933@gmail.com>
2022-05-26 22:39:42 +0600

    added portfolio

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -3,7 +3,29 @@ import React from 'react';
 const Portfolio = () => {
     return (
         <div>
-            <h1>Portfolio</h1>
+            <div class=" bg-base-200 text-center py-16">
+
+                <h1 class="text-4xl font-bold text-accent">Nargis Akther</h1>
+                <p class="py-3 text-xl font-bold">Email Address:nargisakther1933@gmail.com</p>
+                <p class="py-3 text-xl font-bold">Education: Diploma in Computer Engineering</p>
+                <p class="py-3 text-xl font-bold">Institute: Chattogram Mohila Polytechnic Institute </p>
+                <p class="py-3 text-xl font-bold">Technologies I Know:
+                    <ul>
+                        <li>Html</li>
+                        <li>Css</li>
+                        <li>Bootsrap</li>
+                        <li>Tailwind</li>
+                        <li>React JS</li>
+                        <li>Firebase</li>
+                        <li>Node JS</li>
+                        <li>MongoDB</li>
+                    </ul>
+                </p>
+                <p class="py-3 text-xl font-bold">Here My 3 best application live link:</p>
+                <p>Laptop Mart: https://laptop-mart.web.app/</p>
+                <p>Gym Trainer: https://gym-trainer-d8b35.web.app/</p>
+                <p>Laptop Mart: https://standard-bank-nargis21.netlify.app/</p>
+            </div>
         </div>
     );
 };
