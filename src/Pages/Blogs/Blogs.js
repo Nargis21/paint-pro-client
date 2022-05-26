01a6fd7a876ba6a77a763b7789713a67bd56f88d
Nargis21 <nargisakther1933@gmail.com>
2022-05-26 23:20:45 +0600

    update

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -4,10 +4,15 @@ const Blogs = () => {
     return (
         <div>
             <h1 className='text-2xl text-accent text-bold'>How will you improve the performance of a React Application?</h1>
+            <h1 className='text-bold text-xl'></h1>
             <h1 className='text-2xl text-accent text-bold'>What are the different ways to manage a state in a React application?</h1>
+            <h1 className='text-bold text-xl'></h1>
             <h1 className='text-2xl text-accent text-bold'>Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</h1>
+            <h1 className='text-bold text-xl'></h1>
             <h1 className='text-2xl text-accent text-bold'>How does prototypical inheritance work?</h1>
+            <h1 className='text-bold text-xl'></h1>
             <h1 className='text-2xl text-accent text-bold'>What is a unit test? Why should write unit tests?</h1>
+            <h1 className='text-bold text-xl'></h1>
         </div>
     );
 };
